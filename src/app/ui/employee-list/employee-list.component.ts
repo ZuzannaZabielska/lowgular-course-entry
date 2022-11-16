1e0fa53914b7ff7e09feb05c1ab5446306f4ab56
Zuza <zabielskazu@gmail.com>
2022-11-16 18:39:56 +0100

    feat: title added

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,10 +1,7 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { EmployeeModel } from '../../model/employee.model';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EmployeeModel } from '../../../../../src/app/model/employee.model';
 
 @Component({
   selector: 'employee-list',
@@ -13,5 +10,7 @@ import { EmployeeModel } from '../../model/employee.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  data$: Observable<EmployeeModel[] | null> = of(null);
+  constructor(private _httpClient: HttpClient) {}
+  data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
 }
+
